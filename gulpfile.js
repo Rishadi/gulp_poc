7fbe763dd1634155789100af4f11481ee665ee1a
Rishadi Dahanayake <rishadi2000@gmail.com>
2018-07-15 11:35:10 +1200

    Initialise project shopify

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-
-gulp.task('default', function() {
-  // place code for your default task here
+var watch = require('gulp-watch');
+ 
+gulp.task('watch', function () {
+    return watch('scss/styles.scss', { ignoreInitial: false })
+    .pipe(sass())
+    .pipe(gulp.dest('css'));
 });
 
+gulp.task('task-name', function() {
+  return gulp.src('source-files') // Get source files with gulp.src
+    .pipe(aGulpPlugin()) // Sends it through a gulp plugin
+    .pipe(gulp.dest('destination')) // Outputs the file in the destination folder
+})
+  // place code for your default task here
+
 gulp.task('sass', function(){
-  return gulp.src('app/scss/styles.scss')
+  return gulp.src('scss/styles.scss')
     .pipe(sass()) // Using gulp-sass
-    .pipe(gulp.dest('app/css'))
+    .pipe(gulp.dest('css'))
 });
